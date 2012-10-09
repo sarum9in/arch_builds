68fcd4c84c78a51dbaa56255e5404c0f1378b342
Aleksey Filippov <sarum9in@gmail.com>
2012-10-09 23:58:04 +0400

    qemu-scripts package was provided.

---
+++ qemu-scripts/qemu-scripts/srv/kvm/examples/config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+####################################################
+# example config.sh file for virtual machine #
+####################################################
+
+# you can override qemu command here
+#qemu=('qemu-system-x86_64')
+
+# set up mac address here if you want it to be static
+#macaddr="DE:AD:BE:EF:00:00"
+
+# set up bridge interface if you do not want to use default
+#export QEMU_BRIDGE="br0"
---
+++ qemu-scripts/qemu-scripts/srv/kvm/run-qemu.sh
@@ -0,0 +1,42 @@
+ #!/bin/bash -ex
+
+on_exit()
+{
+    if [[ $pid ]]
+    then
+        echo "terminating $pid" >&2
+        kill -TERM "$pid"
+        wait "$pid"
+    fi
+    sudo tunctl -d "$iface"
+    exit
+}
+trap on_exit TERM INT EXIT
+
+config="$1"
+. "$config"
+shift
+
+userid="$(whoami)"
+iface="$(sudo tunctl -b -u $userid)"
+
+root="$(dirname "$0")"
+if [[ ${root:0:1} != / ]]
+then
+    root="$PWD/$root"
+fi
+
+if [[ -z ${qemu[*]} ]]
+then
+    qemu=('qemu-kvm' '-enable-kvm')
+fi
+
+if [[ -z $macaddr ]]
+then
+    macaddr="$(printf 'DE:AD:BE:EF:%02X:%02X\n' $((RANDOM%256)) $((RANDOM%256)))"
+fi
+
+"${qemu[@]}" -net nic,macaddr="$macaddr",model=rtl8139 -net tap,ifname="$iface",script="$root/qemu-ifup.sh" "$@" &
+pid="$!"
+
+wait "$pid"
