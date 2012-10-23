92b702a3e0f499007e1cb1fa0aaf4a86dbdc1db2
Aleksey Filippov <sarum9in@gmail.com>
2012-10-23 16:02:06 +0400

    scripts/lib/makechrootpkg.sh: infinite loop fix.

--- scripts/lib/makechrootpkg.sh
+++ scripts/lib/makechrootpkg.sh
@@ -6,8 +6,14 @@ make_chroot_pkg()
 {
     makechrootpkg -r "$chroot" -d -- "$@"
     repo_sync "$user"
+    failed=0
     while ! chroot_run "pacman --noconfirm -Sw $(source ./PKGBUILD && echo "$pkgname")" "$user"
     do
-        true
+        failed=$((failed + 1))
+        if (( failed >= 10 ))
+        then
+            echo "Unable to build ${PWD#$root}" >&2
+            return 1
+        fi
     done
 }
