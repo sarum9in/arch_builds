5c0e5453494dce01fcfa532bcbcb3810ae0c0366
Aleksey Filippov <sarum9in@gmail.com>
2013-01-02 23:59:19 +0400

    scripts/repo-add.sh: ^${pkg} syntax support (aur package).

--- scripts/repo-add.sh
+++ scripts/repo-add.sh
@@ -8,37 +8,48 @@ rm -rf "$chroot/$user" "$chroot/$user.lock"
 
 name="mirror.cs.istu.ru"
 
-for pkg in 'bunsan' '~yandex-contest/invoker' 'bunsan/bacs/single' 'qemu-scripts' 'bacs/legacy/userlibs' #'obnam-bzr'
-do
+build()
+{
+    local pkg="$1"
     if [[ ${pkg:0:1} = '~' ]]
     then
         pushd "${pkg:1}/.."
         for i in `tsort <dependencies`
         do
-            pushd "$i"
-            make_chroot_pkg
-            popd
+            build "$i"
             if [[ $(basename "$pkg") = $i ]]
             then
                 break
             fi
         done
         popd
+    elif [[ ${pkg:0:1} = '^' ]]
+    then
+        local tmp="$(mktemp -d)"
+        pushd "$tmp"
+        yaourt -G "${pkg:1}"
+        cd "${pkg:1}"
+        make_chroot_pkg
+        popd
+        rm -rf "$tmp"
     else
         pushd "${pkg}"
         if [[ -f dependencies ]]
         then
             for i in `tsort <dependencies`
             do
-                pushd "$i"
-                make_chroot_pkg
-                popd
+                build "$i"
             done
         else
             make_chroot_pkg
         fi
         popd
     fi
+}
+
+for pkg in 'bunsan' '~yandex-contest/invoker' 'bunsan/bacs' 'qemu-scripts' 'bacs/legacy/userlibs' #'obnam-bzr'
+do
+    build "$pkg"
 done
 
 pushd "$chroot/$user/repo"
