1080cef4b6ef359630298abbfe1665b10d45c87b
Aleksey Filippov <sarum9in@gmail.com>
2013-02-21 15:18:39 +0400

    bacs_legacy_userlibs package update.

--- bacs/legacy/userlibs/depthlib.h
+++ bacs/legacy/userlibs/depthlib.h
@@ -2,90 +2,107 @@
 
 struct ImBAnode
 {
-	int data;
-	ImBAnode *next;
+    int data;
+    ImBAnode *next;
 };
 
 struct ImBAnode2
 {
-	int data;
-	ImBAnode2 *pred;
-	ImBAnode2 *next;
+    int data;
+    ImBAnode2 *pred;
+    ImBAnode2 *next;
 };
 
 struct ImBATreenode
 {
-	int data;
-	ImBATreenode *left;
-	ImBATreenode *right;
+    int data;
+    ImBATreenode *left;
+    ImBATreenode *right;
 };
 
 struct ImBAListnode
 {
-	ImBAnode *data;
-	int in;
-	ImBAListnode *next;
+    ImBAnode *data;
+    int in;
+    ImBAListnode *next;
 };
 
 void PrintList( void *q )
 {
-	ImBAnode *p = (ImBAnode *)q;
-	while ( p != 0 )
-	{
-		printf( "BUG0GA %d\n", p->data );
-		p = p->next;
-	}
-	return;
+    ImBAnode *p = (ImBAnode *)q;
+    while ( p != 0 )
+    {
+        printf( "BUG0GA %d\n", p->data );
+        p = p->next;
+    }
+    return;
 }
 
 void PrintList2( void *q )
 {
-	ImBAnode2 *p = (ImBAnode2 *)q;
-	while ( p != 0 )
-	{
-		printf( "AT@TA %d\n", p->data );
-		p = p->next;
-	};
-	return;
+    ImBAnode2 *p = (ImBAnode2 *)q;
+    while ( p != 0 )
+    {
+        printf( "AT@TA %d\n", p->data );
+        p = p->next;
+    };
+    return;
 }
 
 void PrintListList( void *q )
 {
-	ImBAListnode *p = (ImBAListnode *)q;
-	ImBAnode *w;
-	while ( p != 0 )
-	{
-		w = p->data;
-		printf( "L15T%d", p->in );
-		while ( w != 0 )
-		{
-			printf( "%3d", w->data );
-			w = w->next;
-		}
-		printf( "\n" );
-		p = p->next;
-	};
-	return;
+    ImBAListnode *p = (ImBAListnode *)q;
+    ImBAnode *w;
+    while ( p != 0 )
+    {
+        w = p->data;
+        printf( "L15T%d", p->in );
+        while ( w != 0 )
+        {
+            printf( "%3d", w->data );
+            w = w->next;
+        }
+        printf( "\n" );
+        p = p->next;
+    };
+    return;
 }
 
 void ImbaPrintTree( ImBATreenode *q, int g )
 {
-	if ( q == 0 )
-	{
-		printf( "Tr33 %d\n", g );
-	}
-	else
-	{
-		ImbaPrintTree( q->left, g + 1 );
-		printf( "%d %d\n", g, q->data );
-		ImbaPrintTree( q->right, g + 1 );
-	}
-	return;
+    if ( q == 0 )
+    {
+        printf( "Tr33 %d\n", g );
+    }
+    else
+    {
+        ImbaPrintTree( q->left, g + 1 );
+        printf( "%d %d\n", g, q->data );
+        ImbaPrintTree( q->right, g + 1 );
+    }
+    return;
 }
 
 void PrintTree( void *q )
 {
-	ImbaPrintTree( (ImBATreenode *)q, 0 );
-	return;
+    ImbaPrintTree( (ImBATreenode *)q, 0 );
+    return;
 }
 
+struct ImBAnode2
+{
+    int data;
+    ImBAnode2 *pred;
+    ImBAnode2 *next;
+};
+
+void PrintList2rev( void *q )
+{
+    ImBAnode2 *p = (ImBAnode2 *)q;
+    while ( p != 0 )
+    {
+        printf( "AT@TA %d\n", p->data );
+        p = p->pred;
+    };
+    return;
+}
--- bacs/legacy/userlibs/pqueue.h
+++ bacs/legacy/userlibs/pqueue.h
@@ -2,46 +2,46 @@
 
 void print_queue2(void *q)
 {
-	struct State
-	{
-		int ip, next1, next, v;
-		char com[5];
-	};
+    struct State
+    {
+        int ip, next1, next, v;
+        char com[5];
+    };
 
-	struct GG
-	{
-		State state1, state2;
-		int vars;
-	};
+    struct GG
+    {
+        State state1, state2;
+        int vars;
+    };
 
-	struct List
-	{
-		GG data;
-		List *next;
-	};
+    struct List
+    {
+        GG data;
+        List *next;
+    };
 
-	List *queue = (List *)q;
-	List *t;
-	for (t = queue; t != NULL; t = t->next)
-		printf("|%d %d %d|", t->data.state1.ip, t->data.state2.ip, t->data.vars);
+    List *queue = (List *)q;
+    List *t;
+    for (t = queue; t != NULL; t = t->next)
+        printf("|%d %d %d|", t->data.state1.ip, t->data.state2.ip, t->data.vars);
 }
 
 void print_queue1(void *q)
 {
-	struct State
-	{
-		int ip, next1, next, v;
-		char com[5];
-	};
+    struct State
+    {
+        int ip, next1, next, v;
+        char com[5];
+    };
 
-	struct List
-	{
-		State data;
-		List *next;
-	};
+    struct List
+    {
+        State data;
+        List *next;
+    };
 
-	List *queue = (List *)q;
-	List *t;
-	for (t = queue; t != NULL; t = t->next)
-		printf("|%d|", t->data.ip);
+    List *queue = (List *)q;
+    List *t;
+    for (t = queue; t != NULL; t = t->next)
+        printf("|%d|", t->data.ip);
 }
