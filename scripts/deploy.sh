503e8d088bc5566f1c091fd5bab5c2b3700463fb
Aleksey Filippov <sarum9in@gmail.com>
2013-04-15 20:28:29 +0400

    scripts/deploy.sh implementation.

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+source "$(dirname "$0")/lib/root.sh"
+source "$(dirname "$0")/lib/makechrootpkg.sh"
+source "$(dirname "$0")/lib/cdroot.sh"
+
+if [[ -f "$1/${repo_name}.db" ]]
+then
+    rm -f "$1/"*
+    tar xf "${repo_name}.tar" -C "$1"
+else
+    echo "Will not override not repository!" >&2
+    exit 1
+fi
