4277c16a912509a4a52efca74c4274958807a158
Aleksey Filippov <sarum9in@gmail.com>
2015-06-18 00:31:22 +0400

    Added pre-commit hook.

--- scripts/aur.sh
+++ scripts/aur.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -e
 
 script="$(readlink -f "$0")"
+hook="$(dirname "$script")/pre-commit.sh"
 
 if [[ -f .git ]]
 then
-    url="ssh://aur@aur4.archlinux.org/$(basename "$PWD").git"
+    module="$(basename "$PWD")"
+    url="ssh://aur@aur4.archlinux.org/${module}.git"
     if git remote add aur "$url" &>/dev/null
     then
         echo "Added aur = $url"
@@ -12,6 +14,10 @@ then
         git remote set-url aur "$url"
         echo "Fixed aur = $url"
     fi
+    gitdir="$(sed -r 's|^gitdir: (.*)$|\1|' <.git)"
+    rm -f "$gitdir/hooks/pre-commit"
+    cp -f "$hook" "$gitdir/hooks/pre-commit"
+    echo "Added $gitdir/hooks/pre-commit hook"
 else
     git submodule foreach "$script"
 fi
