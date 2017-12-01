03b298c0d81283292126e74a0c62894ad9abf955
Aleksey Filippov <sarum9in@gmail.com>
2017-12-01 01:09:33 +0000

    Cleanup repository before archiving and save files as root:root.

--- scripts/repo-add.sh
+++ scripts/repo-add.sh
@@ -121,5 +121,12 @@ build-go \
     "manul" git+https://github.com/kovetskiy/manul/...
 
 # Finalize repository creation
-tar cf "$root/${repo_name}.tar" -C "$chroot/$user/repo" .
+rm -f "$chroot/$user/repo/${repo_name}.db.tar.gz.old"
+rm -f "$chroot/$user/repo/${repo_name}.db.tar.gz.old.sig"
+rm -f "$chroot/$user/repo/${repo_name}.files.tar.gz.old"
+tar cf "$root/${repo_name}.tar" \
+    --directory="$chroot/$user/repo" \
+    --owner=root:0 \
+    --group=root:0 \
+    .
 update_reporel
